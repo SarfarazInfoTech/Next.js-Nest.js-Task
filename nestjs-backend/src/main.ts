004e3c114e96fc089fbd97107faf4c16f785c6b2
Sarfaraz Khan <sarfarazkhan2020@hotmail.com>
2025-02-19 01:43:27 +0530

    done deploy

--- nestjs-backend/src/main.ts
+++ nestjs-backend/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
   app.enableCors({
-    origin: 'http://localhost:3001',
+    origin: ['http://localhost:3001', 'https://next-js-nest-js-task.vercel.app'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
