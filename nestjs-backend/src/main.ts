3488ddbd69aebd4062fc749431820f52450959e1
Sarfaraz Khan <sarfarazkhan2020@hotmail.com>
2025-02-19 00:15:31 +0530

    port done

--- nestjs-backend/src/main.ts
+++ nestjs-backend/src/main.ts
@@ -3,13 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  const port = process.env.PORT || 3000;
   app.enableCors({
     origin: 'http://localhost:3001',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
 
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
