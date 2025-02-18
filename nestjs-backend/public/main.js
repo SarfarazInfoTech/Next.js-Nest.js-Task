81639e179af87bb569a3d1014888fcf8721e6a60
Sarfaraz Khan <sarfarazkhan2020@hotmail.com>
2025-02-19 01:17:43 +0530

    build done

--- nestjs-backend/public/main.js
+++ nestjs-backend/public/main.js
@@ -4,12 +4,13 @@ const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    const port = process.env.PORT || 3000;
     app.enableCors({
         origin: 'http://localhost:3001',
         methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
         credentials: true,
     });
-    await app.listen(3000);
+    await app.listen(port);
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
